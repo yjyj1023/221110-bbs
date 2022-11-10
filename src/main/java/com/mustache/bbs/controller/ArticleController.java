85f6526cc9e1701b3f9b884204377aad30916c55
yjyj1023 <lyj19961023@gmail.com>
2022-11-10 11:17:15 +0900

    new 컨트롤러 생성

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -1,4 +1,16 @@
 package com.mustache.bbs.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/articles")
 public class ArticleController {
+
+    @GetMapping(value = "/new")
+    public String newArticleFrom(){
+        return "/new";
+    }
 }
