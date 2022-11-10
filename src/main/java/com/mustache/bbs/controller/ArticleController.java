ea51c2d4bfcb2127cd2b39593e073a6ef80345ed
yjyj1023 <lyj19961023@gmail.com>
2022-11-10 13:13:27 +0900

    selectSingle 생성

--- src/main/java/com/mustache/bbs/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs/controller/ArticleController.java
@@ -6,9 +6,13 @@ import com.mustache.bbs.domain.entity.ArticleEntity;
 import com.mustache.bbs.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
@@ -32,5 +36,15 @@ public class ArticleController {
         ArticleEntity articleEntity = form.toEntity();
         articleRepository.save(articleEntity);
         return "";
+    }
+
+    @GetMapping(value = "/{id}")
+    public String selectSingle(@PathVariable Long id, Model model){
+        Optional<ArticleEntity> optArticle = articleRepository.findById(id);
+        if(!optArticle.isEmpty()){
+            return "show";
+        }else{
+            return "error";
+        }
     }
 }
