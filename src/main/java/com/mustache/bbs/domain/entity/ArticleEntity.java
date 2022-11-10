00703ae1a06284bc9de23fc72af39753273396e1
yjyj1023 <lyj19961023@gmail.com>
2022-11-10 13:11:11 +0900

    ArticleEntity에 테이블 이름 지정해줌

--- src/main/java/com/mustache/bbs/domain/entity/ArticleEntity.java
+++ src/main/java/com/mustache/bbs/domain/entity/ArticleEntity.java
@@ -3,16 +3,15 @@ package com.mustache.bbs.domain.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "article")
 public class ArticleEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String content;
